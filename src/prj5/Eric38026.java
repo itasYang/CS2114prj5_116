3764825bad7241f165005425d6e7c8d3c4983552
Yuka~Gravity！ <Yang_Yuxiao@outlook.com>
2025-04-10 18:18:04 -0400

    Update Eric38026.java

--- src/prj5/Eric38026.java
+++ src/prj5/Eric38026.java
@@ -1,5 +1,12 @@
 package prj5;
 
-public class Eric38026 {
+public class Eric38026
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("Glad to be on your team");
+
+    }
 
 }
